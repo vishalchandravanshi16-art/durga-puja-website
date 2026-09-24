9d8794d2653622519efc03dc340ae253055c8a77
vishalchandravanshi16-art <vishalchandravanshi16@gmail.com>
2026-09-24 16:12:34 +0530

    update api base url for render

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: '/api'
+  baseURL: 'https://durga-puja-app-2026.onrender.com/api'
 });
 
 // Attach JWT token to every request if available
